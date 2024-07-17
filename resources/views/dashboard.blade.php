c37c9d37071749bd0c30ab843644f2957b23ec01
Postich-prog <mslutskikh@bk.ru>
2024-07-17 20:38:48 +0300

    remake routs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,13 +23,11 @@
                 <div class="modal" id="myModal">
                     <div class="modal-dialog">
                         <div class="modal-content">
-                            <!-- Modal Header -->
                             <div class="modal-header">
-                                <h4 class="modal-title">Добавление новой таблицы</h4>
+                                <h4 class="modal-title">Добавление таблицы</h4>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                             </div>
 
-                            <!-- Modal body -->
                             <div class="modal-body">
                                 <form action="{{ route('csv.store') }}" method="POST" enctype="multipart/form-data">
                                     @csrf
@@ -38,7 +36,6 @@
                                 </form>
                             </div>
 
-                            <!-- Modal footer -->
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Закрыть</button>
                             </div>
